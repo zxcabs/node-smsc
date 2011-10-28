1023f816a8488a56710a319ecef615fea17a1ee9
Evgeny Reznichenko <kusakyky@gmail.com>
2011-10-28 15:25:57 +0400

    add extend

---
+++ test/test_utils.js
@@ -0,0 +1,14 @@
+//utils test;
+
+var utils = require('../lib/utils'),
+	assert = require('assert');
+
+module.exports = {
+	'test extend #1': function () {
+		var a = {}, b = { foo: 1};
+		
+		a = utils.extend(a, b);
+			debugger;
+		assert.deepEqual(a, b);
+	}
+};
