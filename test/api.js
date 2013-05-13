1913c1485668a1ab0e6a5cfcc055bbcbb0399bcd
Evgeny Reznichenko <kusakyky@gmail.com>
2013-05-13 14:08:13 +0400

    add first test for api.balance

--- test/api.js
+++ test/api.js
@@ -6,11 +6,29 @@
  */
 
 var srv = require('./common/srv.js'),
-	should = require('should');
+	should = require('should'),
+	api = require('../lib/api.js'),
+	LOGIN = require('./common/auth.js').LOGIN,
+	PASSWORD = require('./common/auth.js').PASSWORD;
 
 /**
  * Test for API
  */
 describe('API', function () {
 
+	/**
+	 * Test for balnce
+	 */
+	describe('balnce', function () {
+
+		it('should return balance', function (done) {
+			api(LOGIN, PASSWORD)
+				.balance()
+				.exec(function (err, result) {
+					should.not.exist(err);
+					should.exist(result);
+					done();
+				});
+		});
+	});
 });
